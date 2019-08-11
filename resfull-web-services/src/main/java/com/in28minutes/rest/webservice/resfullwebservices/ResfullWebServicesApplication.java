feecbd8676984984f8c44cf0f7cb6fbfed034f77
Bibhudatta Singh <tech.bibhu@gmail.com>
2019-08-11 18:41:35 +0530

    APPCLASS MODIFIED

--- resfull-web-services/src/main/java/com/in28minutes/rest/webservice/resfullwebservices/ResfullWebServicesApplication.java
+++ resfull-web-services/src/main/java/com/in28minutes/rest/webservice/resfullwebservices/ResfullWebServicesApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 public class ResfullWebServicesApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welll come BRAVO");
 		System.out.println("Welll come to Spring Boot microservice..");
 		SpringApplication.run(ResfullWebServicesApplication.class, args);
 	}
